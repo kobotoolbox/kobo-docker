11316f6c291af89c0a111af5284531d4950e2d06
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-22 12:05:16 -0500

    Simplify configuring Nginx for debugging KoBo apps locally.

--- kobo_local/nginx_command.bash
+++ kobo_local/nginx_command.bash
@@ -1,18 +1,59 @@
 #!/bin/bash
 
+ORIGINAL_DIR='/tmp/kobo_nginx'
+TEMPLATES_ENABLED_DIR='/tmp/nginx_templates_activated'
+
+mkdir -p ${TEMPLATES_ENABLED_DIR}
+
+templated_var_refs='${KOBOCAT_PUBLIC_PORT} ${KOBOFORM_PUBLIC_PORT} ${KPI_PUBLIC_PORT} ${ENKETO_EXPRESS_PUBLIC_PORT}'
+
 echo "Clearing out any default configurations."
 rm -rf /etc/nginx/conf.d/*
 
-# Do environment variable substitutions.
-cp /tmp/nginx_site_http.conf.tmpl /tmp/nginx_site_http.conf.tmp
-for varname in KOBOCAT_PUBLIC_PORT KOBOFORM_PUBLIC_PORT KPI_PUBLIC_PORT ENKETO_EXPRESS_PUBLIC_PORT; do
-    cat /tmp/nginx_site_http.conf.tmp | envsubst "'\${$varname}'" > /tmp/nginx_site_http.conf.swp
-    mv /tmp/nginx_site_http.conf.swp /tmp/nginx_site_http.conf.tmp
-done
-cp /tmp/nginx_site_http.conf.tmp /etc/nginx/conf.d/kobo_site_http.conf
+declare -A container_ports
+container_ports=( ['kpi']='8000' ['dkobo']='8000' ['kobocat']='8000' )
+for container_name in "${!container_ports[@]}"; do
 
+    debug_varname="NGINX_DEBUG_${container_name}"     # E.g. `NGINX_DEBUG_kpi`.
+    if [[ "${!debug_varname}" == 'True' ]] ; then
+        echo "Debug proxying directly to \`${container_name}\` without uWSGI."
+
+        # Create a `proxy_pass` configuration for this container.
+        cat ${ORIGINAL_DIR}/proxy_pass.conf.tmpl \
+            | container_name="${container_name}" envsubst '${container_name}' \
+            | container_port="${container_ports[${container_name}]}" envsubst '${container_port}' \
+            > ${TEMPLATES_ENABLED_DIR}/${container_name}_proxy_pass.conf
+
+        # Prepare to include the generated `proxy_pass` config. and no `uwsgi_pass` config.
+        export ${container_name}_include_proxy_pass="include ${TEMPLATES_ENABLED_DIR}/${container_name}_proxy_pass.conf;"
+        export ${container_name}_include_uwsgi_pass=''
+    else
+        echo "Proxying to \`${container_name}\` through uWSGI."
+
+        # Create a `uwsgi_pass` configuration for this container.
+        cat ${ORIGINAL_DIR}/uwsgi_pass.conf.tmpl \
+            | container_name="${container_name}" envsubst '${container_name}' \
+            | container_port="${container_ports[$container_name]}" envsubst '${container_port}' \
+            > ${TEMPLATES_ENABLED_DIR}/${container_name}_uwsgi_pass.conf
+ 
+        # Prepare to include the generated `uwsgi_pass` config. and no `proxy_pass` config.
+        export ${container_name}_include_proxy_pass=''
+        export ${container_name}_include_uwsgi_pass="include ${TEMPLATES_ENABLED_DIR}/${container_name}_uwsgi_pass.conf;"
+    fi
+
+    # Register the include directive variables (e.g. `kpi_include_proxy_pass` and `kpi_include_uwsgi_pass`)
+    #   for template substitution.
+    templated_var_refs+=" \${${container_name}_include_proxy_pass} \${${container_name}_include_uwsgi_pass}"
+done
+
+# Do environment variable substitutions and activate the resulting config. file.
+cat /tmp/nginx_site_http.conf.tmpl | envsubst "${templated_var_refs}" > /etc/nginx/conf.d/kobo_site_http.conf
+
+# Start Nginx.
 nginx &
 nginx_pid=$!
+
+# When quitting, ensure an accurate exit code is reported.
 trap "echo 'SIGTERM recieved. Killing Nginx.' && kill -SIGTERM ${nginx_pid}" SIGTERM
 wait "${nginx_pid}"
 exit $(($? - 128 - 15)) # http://unix.stackexchange.com/questions/10231/when-does-the-system-send-a-sigterm-to-a-process#comment13523_10231
