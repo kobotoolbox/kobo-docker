66f5be62649df8af03d06f793c8207f927ddf7f2
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-05-04 12:13:11 -0400

    Automatically back up Postgres 9.3 database before upgrading.

--- base_images/psql/init_postgres.bash
+++ base_images/psql/init_postgres.bash
@@ -10,25 +10,32 @@ PSQL_CONFIG_FILE=/etc/postgresql/9.4/main/postgresql.conf
 PSQL_DATA=/srv/db
 
 if [[ "$(cat /srv/db/PG_VERSION)" == '9.3' ]]; then
-    echo 'Existing Postgres 9.3 database detected. Preparing to upgrade it.'
+    echo 'Existing Postgres 9.3 database cluster detected. Preparing to upgrade it.'
     echo 'Installing Postgres 9.3 and dependencies.'
     apt-get -qq update
     apt-get install -qqy postgresql-9.3-postgis-2.1
-    echo 'Removing automatically-installed 9.3 default database.'
+    echo 'Removing Postgres 9.3 automatically-installed default cluster.'
     pg_dropcluster 9.3 main
-    echo 'Moving old database contents to `/srv/db_9.3/`.'
+    echo 'Moving old cluster contents to `/srv/db_9.3/`.'
     mkdir -p /srv/db_9.3
     chmod --reference=/srv/db /srv/db_9.3
     chown --reference=/srv/db /srv/db_9.3
     mv /srv/db/* /srv/db_9.3/
-    echo 'Setting 9.3 to manage the old database.'
+    echo 'Setting Postgres 9.3 to manage the old cluster.'
     mv /etc/postgresql/9.4 /etc/postgresql/9.3
     sed -i 's/9\.4/9\.3/g' /etc/postgresql/9.3/main/postgresql.conf
     sed -i 's/\/srv\/db/\/srv\/db_9.3/g' /etc/postgresql/9.3/main/postgresql.conf
-    echo 'Executing database upgrade.'
-    pg_upgradecluster 9.3 main /srv/db
+    echo "Creating \`pg_restore\`-compatible, compressed backup of the old \`${KOBO_PSQL_DB_NAME}\` database."
+    TIME_STAMP="$(date +%Y.%m.%d.%H_%M_%S)"
+    pg_ctlcluster 9.3 main start -o '-c listen_addresses=""' # Temporarily start Postgres for local connections only.
+    sudo -u postgres pg_dump -Z1 -Fc "${KOBO_PSQL_DB_NAME}" > "/srv/backups/${TIME_STAMP}__${KOBO_PSQL_DB_NAME}.pg_restore"
+    pg_ctlcluster 9.3 main stop
+    echo 'Executing cluster upgrade (without allowing remote connections).'
+    pg_upgradecluster -o '-c listen_addresses=""' -O '-c listen_addresses=""' 9.3 main /srv/db
     pg_ctlcluster 9.4 main stop
-    echo 'Database upgrade complete.'
+    echo 'Removing old cluster.'
+    pg_dropcluster 9.3 main
+    echo 'Database cluster upgrade complete.'
 fi
 
 # prepare data directory and initialize database if necessary
