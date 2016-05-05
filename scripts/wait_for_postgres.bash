f10718160ee824a8a3ba85dd696c4698c4b25346
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-05-05 08:49:58 -0400

    Take ownership of Postgres container.

--- scripts/wait_for_postgres.bash
+++ scripts/wait_for_postgres.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-echo 'Waiting for container `psql`.'
-dockerize -timeout=20s -wait ${PSQL_PORT}
-echo 'Container `psql` up.'
+echo 'Waiting for container `postgres`.'
+dockerize -timeout=20s -wait ${POSTGRES_PORT}
+echo 'Container `postgres` up.'
 
 echo 'Waiting for Postgres service.'
 # TODO: There must be a way to confirm Postgres is serving without the resulting "incomplete startup packet" warning in the logs.
-KOBO_PSQL_DB_NAME=${KOBO_PSQL_DB_NAME:-"kobotoolbox"}
-KOBO_PSQL_DB_USER=${KOBO_PSQL_DB_USER:-"kobo"}
-until pg_isready -d "${KOBO_PSQL_DB_NAME}" -h psql -U "${KOBO_PSQL_DB_USER}"; do
+KOBO_POSTGRES_DB_NAME=${KOBO_POSTGRES_DB_NAME:-"kobotoolbox"}
+KOBO_POSTGRES_USER=${KOBO_POSTGRES_USER:-"kobo"}
+until pg_isready -d "${KOBO_POSTGRES_DB_NAME}" -h postgres -U "${KOBO_POSTGRES_USER}"; do
     sleep 1
 done
 echo 'Postgres service ready.'
