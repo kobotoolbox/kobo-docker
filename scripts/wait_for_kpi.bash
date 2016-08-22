e735b881bcb81d04c33084a44c5abfa3c34a965c
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-08-22 15:09:50 -0400

    Fix a typo and make `wait_for_kpi.bash` work when KPI isn't running uWSGI.

--- scripts/wait_for_kpi.bash
+++ scripts/wait_for_kpi.bash
@@ -5,9 +5,9 @@ echo 'Waiting for container `kpi`.'
 dockerize -timeout=60s -wait ${KPI_PORT}
 echo 'Container `kpi` up.'
 
-echo 'Waiting for `kpi` uWSGI service.'
-# Beware the following hardcoded port number...
-until $(curl kpi:8000 2> /dev/null) || [[ $? == 52 ]]; do
+echo 'Waiting for `kpi` web service.'
+# NOTE: Beware the following hardcoded port number...
+until curl kpi:8000 &> /dev/null || [[ "$?" == '52' ]]; do
     sleep 1
 done
-echo '`kpi` uWSGI service ready.'
+echo '`kpi` web service ready.'
