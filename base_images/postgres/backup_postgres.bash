c5118e72eed08f5a1c9d3af1fe7d2a25fdf52668
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-11-15 16:28:28 -0500

    Fix Postgres backups permission issue. Report successful backups.

--- base_images/postgres/backup_postgres.bash
+++ base_images/postgres/backup_postgres.bash
@@ -2,4 +2,6 @@
 set -e
 
 timestamp="$(date +%Y.%m.%d.%H_%M)"
-pg_dump --compress=1 --format=custom ${KOBO_POSTGRES_DB_NAME} > "/srv/backups/postgres_backup_${KOBO_POSTGRES_DB_NAME}__${timestamp}.pg_restore"
+backup_filename="postgres_backup_${KOBO_POSTGRES_DB_NAME}__${timestamp}.pg_restore"
+su - postgres -c "pg_dump --compress=1 --format=custom ${KOBO_POSTGRES_DB_NAME}" > "/srv/backups/${backup_filename}"
+echo "Backup file \`${backup_filename}\` created successfully."
