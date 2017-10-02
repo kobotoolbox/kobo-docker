86906367b93a36b90b429270993719ba71591844
Olivier Léger <oleger@lg2fabrique.com>
2017-10-02 18:00:36 -0400

    Changed 'wait_for...' scripts to handle remote services

--- scripts/wait_for_rabbit.bash
+++ scripts/wait_for_rabbit.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+#echo 'Waiting for container `rabbit`.'
+#dockerize -timeout=40s -wait ${RABBIT_PORT}
+#echo 'Container `rabbit` up.'
+
 echo 'Waiting for container `rabbit`.'
-dockerize -timeout=40s -wait ${RABBIT_PORT}
-echo 'Container `rabbit` up.'
+sleep 40
+IS_OPENED=$((echo > /dev/tcp/rabbit/${RABBIT_PORT//\"/}) >/dev/null 2>&1 && echo "1" || echo "0")
+
+if [ "$IS_OPENED" == "1" ]; then
+    echo 'Container `rabbit` up.'
+else
+    echo 'Container `rabbit` down.'
+fi
