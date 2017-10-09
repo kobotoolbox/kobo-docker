3f6a6843030a191804ec72748f925b985ffb9d3a
Olivier Léger <olivierleger@gmail.com>
2017-10-09 11:12:48 -0400

    Fixed hardcoded postgres hostname in postgres init script

--- scripts/wait_for_postgres.bash
+++ scripts/wait_for_postgres.bash
@@ -9,7 +9,7 @@ echo 'Waiting for Postgres service.'
 # TODO: There must be a way to confirm Postgres is serving without the resulting "incomplete startup packet" warning in the logs.
 KOBO_POSTGRES_DB_NAME=${KOBO_POSTGRES_DB_NAME:-"kobotoolbox"}
 KOBO_POSTGRES_USER=${KOBO_POSTGRES_USER:-"kobo"}
-until pg_isready -d "${KOBO_POSTGRES_DB_NAME}" -h postgres -U "${KOBO_POSTGRES_USER}"; do
+until pg_isready -d "${KOBO_POSTGRES_DB_NAME}" -h ${KOBO_POSTGRES_HOST} -U "${KOBO_POSTGRES_USER}"; do
     sleep 1
 done
 echo 'Postgres service ready.'
