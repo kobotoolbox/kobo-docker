c51289db1005e8ac65ee0c154eb63376baf0f504
Olivier Léger <olivierleger@gmail.com>
2018-11-02 11:47:11 -0400

    Updated deletion rules to match Glacier minimum days of storage

--- postgres/backup-to-s3.py
+++ postgres/backup-to-s3.py
@@ -28,11 +28,14 @@ DUMPFILE = 'postgres-{}-{}-{}.pg_dump'.format(
 )
 BACKUP_COMMAND = 'pg_dump --format=c --dbname="{}"'.format(DBURL)
 
+# If LifeCycle rules are enabled on bucket to move files to Glacier.
+# Keep in mind that file must stay in Glacier for (at least) 90 days.
+# Otherwise early deletion fee will be charged.
 DIRECTORIES = [
-    {'name': 'yearly', 'keeps': 5, 'days': 365},
-    {'name': 'monthly', 'keeps': 12, 'days': 30},
-    {'name': 'weekly', 'keeps': 4, 'days': 7},
-    {'name': 'daily', 'keeps': 30, 'days': 1},
+    {'name': 'yearly', 'keeps': 2, 'days': 365},
+    {'name': 'monthly', 'keeps': 3, 'days': 30},
+    {'name': 'weekly', 'keeps': 13, 'days': 7},
+    {'name': 'daily', 'keeps': 90, 'days': 1},
 ]
 
 # Consider backups invalid whose (compressed) size is below this number of
