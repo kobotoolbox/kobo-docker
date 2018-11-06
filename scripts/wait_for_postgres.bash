2cc882f134529ca41c5b14976d1368a661ceb9dd
Olivier Léger <olivierleger@gmail.com>
2018-11-06 14:49:15 -0500

    Use custom port when testing whether postgres is online

--- scripts/wait_for_postgres.bash
+++ scripts/wait_for_postgres.bash
@@ -9,12 +9,12 @@ echo 'Waiting for Postgres service.'
 # TODO: There must be a way to confirm Postgres is serving without the resulting "incomplete startup packet" warning in the logs.
 POSTGRES_DB_NAME="${POSTGRES_DB_NAME:-kobotoolbox}"
 POSTGRES_USER="${POSTGRES_USER:-kobo}"
-until pg_isready -h "${POSTGRES_HOST}"; do
+until pg_isready -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}"; do
     sleep 1
 done
 echo "Postgres service running; ensuring ${POSTGRES_DB_NAME} database exists and has PostGIS extensions..."
 PGPASSWORD="${POSTGRES_PASSWORD:-kobo}" psql \
-    -d postgres -h "${POSTGRES_HOST}" -U "${POSTGRES_USER}" <<EOF
+    -d postgres -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U "${POSTGRES_USER}" <<EOF
 CREATE DATABASE "$POSTGRES_DB_NAME" OWNER "$POSTGRES_USER";
 \c "$POSTGRES_DB_NAME"
 CREATE EXTENSION IF NOT EXISTS postgis; CREATE EXTENSION IF NOT EXISTS postgis_topology;
