bee6fb1401e7a43c5b518778134f10c5640d8e10
Olivier Léger <olivierleger@gmail.com>
2018-11-19 19:32:37 -0500

    Handle backups for databases (on disk or to S3)

---
+++ mongo/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set -e
+
+BASH_PATH=$(which bash)
+export KOBO_DOCKER_SCRIPTS_DIR=/kobo-docker-scripts
+
+$BASH_PATH $KOBO_DOCKER_SCRIPTS_DIR/toggle-backup-activation.sh
+
+echo "Copying init scripts ..."
+cp $KOBO_DOCKER_SCRIPTS_DIR/init_* /docker-entrypoint-initdb.d/
+
+echo "Launching official entrypoint..."
+$BASH_PATH /entrypoint.sh mongod
