6cf10b8244c4fbf8ba43409c98469dfb36ca3bfc
Olivier Léger <olivierleger@gmail.com>
2018-11-20 18:56:20 -0500

    Keep only one copy of backup when saving on disk

--- postgres/backup-to-disk.bash
+++ postgres/backup-to-disk.bash
@@ -3,6 +3,8 @@ set -e
 
 DBDATESTAMP="$(date +%Y.%m.%d.%H_%M)"
 BACKUP_FILENAME="postgres-${PG_MAJOR}-${PUBLIC_DOMAIN_NAME}-${DBDATESTAMP}.pg_dump"
-pg_dump --format=custom ${POSTGRES_DB} > "/srv/backups/${BACKUP_FILENAME}"
+cd /srv/backups
+rm -rf *.pg_dump
+pg_dump --format=custom ${POSTGRES_DB} > "${BACKUP_FILENAME}"
 
 echo "Backup file \`${BACKUP_FILENAME}\` created successfully."
