7cfd80985027fa9df505b5c7b137412e862fdd49
Olivier Leger <olivierleger@gmail.com>
2020-02-06 09:10:00 -0400

    Create authenticated MongoDB user on DB init

--- mongo/init_01_add_index.sh
+++ mongo/init_01_add_index.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # copyleft 2015 Serban Teodorescu <teodorescu.serban@gmail.com> 
-
 # creates the additional required index for kobo mongo
 
-DB=formhub
+echo "Creating index for ${MONGO_INITDB_DATABASE}..."
+
 COL=instances
 
-echo "db.$COL.createIndex( { _userform_id: 1 } )" | mongo $DB
+echo "db.$COL.createIndex( { _userform_id: 1 } )" | mongo ${MONGO_INITDB_DATABASE}
+
+echo "Done!"
