6723091f12f5fd612d861fee9f7792841ff23dae
Olivier Leger <olivierleger@gmail.com>
2020-02-06 11:51:59 -0400

    Use envsub instead of sed to create redis conf

--- redis/entrypoint.sh
+++ redis/entrypoint.sh
@@ -2,39 +2,46 @@
 
 ORIGINAL_DIR="/tmp/redis"
 REDIS_LOG_DIR="/var/log/redis"
-REDIS_CONF_DIR="/etc/redis/"
+REDIS_CONF_DIR="/etc/redis"
 REDIS_CONF_FILE="${REDIS_CONF_DIR}/redis.conf"
 REDIS_DATA_DIR="/data/"
 
-CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
+export CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
 
-if [ ! -d "$REDIS_LOG_DIR" ]; then
+if [[ ! -d "$REDIS_LOG_DIR" ]]; then
     mkdir -p "$REDIS_LOG_DIR"
 fi
 
-if [ ! -d "$REDIS_DATA_DIR" ]; then
+if [[ ! -d "$REDIS_DATA_DIR" ]]; then
     mkdir -p "$REDIS_DATA_DIR"
 fi
 
 
 # Copy config file
-cp "${REDIS_CONF_FILE}.tmpl" $REDIS_CONF_FILE
+#cp "${REDIS_CONF_FILE}.tmpl" $REDIS_CONF_FILE
 
 # Create redis-server configuration file
-sed -i "s~\${CONTAINER_IP}~${CONTAINER_IP//\"/}~g" "$REDIS_CONF_FILE"
+#sed -i "s~\${CONTAINER_IP}~${CONTAINER_IP//\"/}~g" "$REDIS_CONF_FILE"
 
 # Create redis-server configuration file
-sed -i "s~\${REDIS_PASSWORD}~${REDIS_PASSWORD//\"/}~g" "$REDIS_CONF_FILE"
+#sed -i "s~\${REDIS_PASSWORD}~${REDIS_PASSWORD//\"/}~g" "$REDIS_CONF_FILE"
+
+# install envsubst
+apt-get update && apt-get -y install gettext-base
+
+cat "$REDIS_CONF_FILE.tmpl" \
+      | envsubst '${CONTAINER_IP} ${REDIS_PASSWORD}' \
+        > "$REDIS_CONF_FILE"
 
 # Make logs directory writable
 chown -R redis:redis "$REDIS_LOG_DIR"
 chown redis:redis "$REDIS_CONF_FILE"
 chown -R redis:redis "$REDIS_DATA_DIR"
 
-if [ "${KOBO_REDIS_SERVER_ROLE}" == "main" ]; then
+if [[ "$KOBO_REDIS_SERVER_ROLE" == "main" ]]; then
     BASH_PATH=$(which bash)
     export KOBO_DOCKER_SCRIPTS_DIR=/kobo-docker-scripts
-    $BASH_PATH $KOBO_DOCKER_SCRIPTS_DIR/toggle-backup-activation.sh
+    "$BASH_PATH" "$KOBO_DOCKER_SCRIPTS_DIR/toggle-backup-activation.sh"
 fi
 
 su redis -c "redis-server /etc/redis/redis.conf"
