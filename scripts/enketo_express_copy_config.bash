0947ee532c7c9c39cb9b19a2f446f8d99f6e8f55
John N. Milner <john@tmoj.net>
2020-03-19 01:09:45 -0400

    Upgrade EE to 1.86.2-jnm-docker-ie11

--- scripts/enketo_express_copy_config.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Copying the read-only configuration template into place at \`${ENKETO_SRC_DIR}/config/config.json\`."
-cp /srv/tmp/enketo_express_config.json ${ENKETO_SRC_DIR}/config/config.json
