8717a2540ed3c413f682eb8741243bea59c44633
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2020-04-04 15:52:52 +0700

    Update wait_for_kpi.bash
    Should not fix to 'kpi' but '{KPI_HOST}'

--- scripts/wait_for_kpi.bash
+++ scripts/wait_for_kpi.bash
@@ -2,11 +2,11 @@
 set -e
 
 echo 'Waiting for container `kpi`.'
-dockerize -timeout=300s -wait tcp://kpi:${KPI_PORT}
+dockerize -timeout=300s -wait tcp://{KPI_HOST}:${KPI_PORT}
 echo 'Container `kpi` up.'
 
 echo 'Waiting for `kpi` web service.'
-until curl kpi:${KPI_PORT} &> /dev/null || [[ "$?" == '52' ]]; do
+until curl {KPI_HOST}:${KPI_PORT} &> /dev/null || [[ "$?" == '52' ]]; do
     sleep 1
 done
 echo '`kpi` web service ready.'
