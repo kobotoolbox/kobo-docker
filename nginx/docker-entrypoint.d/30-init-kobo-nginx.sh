c70308924cea8f2146984c369c883efaf6045ee4
Olivier Leger <olivierleger@gmail.com>
2024-04-05 12:47:25 -0400

    Fix NGINX template

--- nginx/docker-entrypoint.d/30-init-kobo-nginx.sh
+++ nginx/docker-entrypoint.d/30-init-kobo-nginx.sh
@@ -13,7 +13,8 @@ cp ${KOBO_DOCKER_SCRIPTS_DIR}/nginx.conf /etc/nginx/nginx.conf
 echo "Clearing out any default configurations"
 rm -rf /etc/nginx/conf.d/*
 
-TEMPLATED_VAR_REFS="${TEMPLATED_VAR_REFS} \${NGINX_PUBLIC_PORT}"
+export TEMPLATED_VAR_REFS="${TEMPLATED_VAR_REFS} \${NGINX_PUBLIC_PORT}"
+export KOBOCAT_HEADER_FOR_DEV=''
 
 if [[ "${WSGI}" != 'UWSGI' ]] ; then
     echo "Proxying directly (debug) to Django without uWSGI."
@@ -22,17 +23,20 @@ if [[ "${WSGI}" != 'UWSGI' ]] ; then
     cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/proxy_pass.conf.tmpl \
         | envsubst '${KOBOFORM_PUBLIC_SUBDOMAIN} ${PUBLIC_DOMAIN_NAME} ${NGINX_PUBLIC_PORT}' \
         > ${INCLUDES_DIR}/proxy_pass.conf
+
+    # Allow to open Kobocat in an iframe
+    export KOBOCAT_HEADER_FOR_DEV='add_header X-Frame-Options ALLOWALL always;'
 else
     echo "Proxying to Django through uWSGI."
 
     # Create a `uwsgi_pass` configuration for this container.
     cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/uwsgi_pass.conf.tmpl \
         | envsubst '${UWSGI_PASS_TIMEOUT}' \
-        > ${INCLUDES_DIR}/uwsgi_pass.conf
+        > ${INCLUDES_DIR}/proxy_pass.conf
 fi
 
 # Do environment variable substitutions and activate the resulting config. file.
-cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/nginx_site_default.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS}" > /etc/nginx/conf.d/default.conf
+cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/nginx_site_default.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS} \${KOBOCAT_HEADER_FOR_DEV}" > /etc/nginx/conf.d/default.conf
 
 # Copy includes files
 cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/include.https_redirection.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS}" > /etc/nginx/includes/https_redirection.conf
