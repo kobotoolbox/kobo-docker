cea9364693730ca671e0112e8946424490be71f5
Olivier Leger <olivierleger@gmail.com>
2024-05-28 18:26:24 -0400

    Remove XFRAME_OPTION header dev environment

--- nginx/docker-entrypoint.d/30-init-kobo-nginx.sh
+++ nginx/docker-entrypoint.d/30-init-kobo-nginx.sh
@@ -14,7 +14,6 @@ echo "Clearing out any default configurations"
 rm -rf /etc/nginx/conf.d/*
 
 export TEMPLATED_VAR_REFS="${TEMPLATED_VAR_REFS} \${NGINX_PUBLIC_PORT}"
-export KOBOCAT_HEADER_FOR_DEV=''
 
 if [[ "${WSGI}" != 'uWSGI' ]] ; then
     echo "Proxying directly (debug) to Django without uWSGI."
@@ -23,9 +22,6 @@ if [[ "${WSGI}" != 'uWSGI' ]] ; then
     cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/proxy_pass.conf.tmpl \
         | envsubst '${KOBOFORM_PUBLIC_SUBDOMAIN} ${PUBLIC_DOMAIN_NAME} ${NGINX_PUBLIC_PORT}' \
         > ${INCLUDES_DIR}/proxy_pass.conf
-
-    # Allow to open Kobocat in an iframe
-    export KOBOCAT_HEADER_FOR_DEV='add_header X-Frame-Options ALLOWALL always;'
 else
     echo "Proxying to Django through uWSGI."
 
@@ -36,7 +32,7 @@ else
 fi
 
 # Do environment variable substitutions and activate the resulting config. file.
-cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/nginx_site_default.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS} \${KOBOCAT_HEADER_FOR_DEV}" > /etc/nginx/conf.d/default.conf
+cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/nginx_site_default.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS}" > /etc/nginx/conf.d/default.conf
 
 # Copy includes files
 cat ${KOBO_DOCKER_SCRIPTS_DIR}/templates/include.https_redirection.conf.tmpl | envsubst "${TEMPLATED_VAR_REFS}" > /etc/nginx/includes/https_redirection.conf
